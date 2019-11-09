708da0da52e1f3f1cf6da93106b3161563eb966f
mgalla28 <35844757+mgalla28@users.noreply.github.com>
2019-11-09 16:39:09 -0600

    Add files via upload

---
+++ scripts/test_compare.py
@@ -0,0 +1,28 @@
+from CourseList import CourseList
+from Course import Course
+
+from pymongo import MongoClient
+import pymongo
+
+conn = MongoClient()
+db = conn.ClassGraphs
+coll = db.CsClasses  # type: pymongo.collection
+
+master_dict = {}
+for i in coll.find({}):
+    master_dict.update({i['designation']: Course(i['designation'], list(i['out_edges']), i['credit_hours'])})
+
+
+cs141 = Course('CS 141', ['CS 251', 'CS 211', 'CS 261'], 3)
+cs151 = Course('CS 151', ['CS 251'], 3)
+cs211 = Course('CS 211', [], 3)
+cs251 = Course('CS 251', [], 3)
+cs261 = Course('CS 261', [], 3)
+
+
+courseDict = {'CS 141': cs141, 'CS 151': cs151, 'CS 211': cs211, 'CS 251': cs251, 'CS 261': cs261}
+
+master = CourseList(master_dict)
+user = CourseList({'CS 141': cs141})
+
+print(master.return_vertices_set().difference(user.return_vertices_set()))
