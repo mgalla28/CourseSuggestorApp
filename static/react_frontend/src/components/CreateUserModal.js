8915ccb4cb82b941dc7ec9ecd6090f59f8845687
michaelgallagher <michaeljgallagher9@gmail.com>
2022-10-07 15:05:35 -0500

    Updated modal style

--- static/react_frontend/src/components/CreateUserModal.js
+++ static/react_frontend/src/components/CreateUserModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { ModalTitle, Modal } from 'react-bootstrap';
+import { ModalTitle, Modal, Button } from 'react-bootstrap';
 import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 
 export default function CreateUserModal() {
@@ -7,6 +7,7 @@ export default function CreateUserModal() {
     const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [secondPassword, setSecondPassword] = useState('');
+
     function openModal() {
         setOpen(true);
     }
@@ -15,23 +16,33 @@ export default function CreateUserModal() {
         setOpen(false);
     }
 
+    const submitUser = async () => {
+
+    }
+
   return (
     <div className="d-flex flex-column">
-    <button className="btn btn-secondary mt-2" onClick={openModal}>Create User</button>
-    <Modal show={open} onHide={closeModal}>
-        <ModalHeader closeButton>
-            <ModalTitle>Create User</ModalTitle>
+        <button className="btn btn-secondary mt-2" onClick={openModal}>Create User</button>
+        <Modal show={open} onHide={closeModal}>
+            <ModalHeader closeButton>
+                <ModalTitle>Create User</ModalTitle>
+            </ModalHeader> 
             <Modal.Body>
-                <label>Username</label>
-                <input onChange={(e) => setUserName(e.target.value)}></input>
-                <label>Password</label>
-                <input type="password" onChange={(e) => setPassword(e.target.value)}></input>
-                <label>Re-enter Password</label>
-                <input type="password" onChange={(e) => setSecondPassword(e.target.value)}></input>
+                <div className="create-user-inputs">
+                    <label>Username</label>
+                    <input onChange={(e) => setUserName(e.target.value)}></input>
+                    <label>Password</label>
+                    <input type="password" onChange={(e) => setPassword(e.target.value)}></input>
+                    <label>Re-enter Password</label>
+                    <input type="password" onChange={(e) => setSecondPassword(e.target.value)}></input>
+                </div>
             </Modal.Body>            
-        </ModalHeader>
-    </Modal>
 
+            <Modal.Footer>
+                <Button variant="secondary" size="lg">Submit</Button>
+            </Modal.Footer>
+            
+        </Modal>
     </div>
   )
 }
